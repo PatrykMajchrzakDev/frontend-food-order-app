0b5835a9581ddfc0656648bfc5939b12b6cae3db
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-08-01 12:26:09 +0200

    Added bump side effect whenever somethings is added to cart

--- src/components/HeaderCart.jsx
+++ src/components/HeaderCart.jsx
@@ -1,12 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../store/cart-context";
 import CartIcon from "../assets/shopping-cart.png";
-import "../styles/HeaderCart.css";
-const HeaderCart = () => {
+import classes from "../styles/HeaderCart.module.css";
+const HeaderCart = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCardItems = cartCtx.items.reduce((prev, curr) => {
+    return prev + curr.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
     <>
-      <button className="button">
-        <img src={CartIcon} alt="" className="icon" />
+      <button className={btnClasses} onClick={props.onClick}>
+        <img src={CartIcon} alt="cartIcon" className={classes.icon} />
         <span>Your Cart</span>
-        <span className="badge">2</span>
+        <span className={classes.badge}>{numberOfCardItems}</span>
       </button>
     </>
   );

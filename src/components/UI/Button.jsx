3671bcaaf8b78e1c8e40f7002020e59ec5f83bf5
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-08-01 12:25:23 +0200

    Removed unnecessary inline styling

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,18 +1,9 @@
 import "../../styles/Button.css";
 
 const Button = (props) => {
-  const customButtonStyles = {
-    color: props.color,
-    ["backgroundColor"]: props.backgroundColor,
-    padding: props.padding,
-  };
   return (
     <>
-      <button
-        className="btn"
-        style={customButtonStyles}
-        onClick={props.onClick}
-      >
+      <button className="btn" onClick={props.onClick}>
         {props.description}
       </button>
     </>

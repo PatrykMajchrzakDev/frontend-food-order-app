47c86d8f764bfd9f56ac4fba3553f343d5743b9c
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-08-07 12:03:04 +0200

    Moved dummy menu list to db and added a way to fetch it from db

--- src/components/MealsForm.jsx
+++ src/components/MealsForm.jsx
@@ -1,35 +1,58 @@
+import { useEffect, useState } from "react";
 import Meal from "./Meals/Meal";
-import "../styles/MealsForm.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import classes from "../styles/MealsForm.module.css";
 
 const MealsForm = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [mealsMenu, setMealsMenu] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-262f1-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMealsMenu(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes["meals-loading"]}>
+        <p>Loading menu...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes["meals-error"]}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+  const mealsList = mealsMenu.map((meal) => (
     <Meal
       key={meal.id}
       id={meal.id}
@@ -40,7 +63,7 @@ const MealsForm = () => {
   ));
 
   return (
-    <section className="form">
+    <section className={classes.form}>
       <ul>{mealsList}</ul>
     </section>
   );
